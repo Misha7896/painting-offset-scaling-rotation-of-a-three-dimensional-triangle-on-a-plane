62e279a7dee061ae718a3e4d9d4ced8efc33541c
Misha7896 <124686856+Misha7896@users.noreply.github.com>
2023-05-28 21:53:30 +0300

    Add files via upload

--- Комп.граф. - сдвиг, поворот, маштаби. треугольника/Комп.граф. №2/Corners.h
+++ Комп.граф. - сдвиг, поворот, маштаби. треугольника/Комп.граф. №2/Corners.h
@@ -13,6 +13,13 @@ public:
 	float y;
 	float z;
 
+	Corners()
+	{
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+	}
+
 	Corners(float x, float y, float z)
 	{
 		this->x = x;
@@ -20,8 +27,22 @@ public:
 		this->z = z;
 	}
 
+	void Konvert(float SHIFT_2)
+	{
+		z = x * SHIFT_2 + y * SHIFT_2 + z;
+	}
+
+	void operator = (Corners& A)
+	{
+		this->x = A.x;
+		this->y = A.y;
+		this->z = A.z;
+	}
+
+
+
 	friend void FOO(Corners& A, Corners& B, Corners& C, float& Cab, float& Cac, float& Cbc, float& Kab1, float& Kab2, float& Bab, float& Kac1, float& Kac2, float& Bac,
-		float& Kbc1, float& Kbc2, float& Bbc, float& Max_x, float& Max_y, float& Min_x, float& Min_y);
+		float& Kbc1, float& Kbc2, float& Bbc, float& Max_x, float& Max_y, float& Min_x, float& Min_y, float SHIFT_2);
 	friend float FOO_SUPPORT(Corners& A, Corners& B, Corners& C, float& Cab, float& Cac, float& Cbc, float& Kab1, float& Kab2, float& Bab, float& Kac1, float& Kac2, float& Bac,
 	float& Kbc1, float& Kbc2, float& Bbc, float& Max_x, float& Max_y, float& Min_x, float& Min_y);
 	friend void FOO2(Corners& A, Corners& B, Corners& C, Corners& D, float& Turn);
